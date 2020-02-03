8d8ba00b8b703ff9d4a5911155423e8e87e99f2e
gnerikuser <57920717+gnerikuser@users.noreply.github.com>
2020-02-03 14:21:39 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<title><?php include "title.txt" ?> by <?php include "author.txt" ?></title>
+<link rel="icon" href="stuff/favicon.ico">
+<link rel="stylesheet" type="text/css" href="stuff/css.css">
+<br></br>
+<br></br>
+<p id="story">     <?php include "title.txt" ?></p>
+<p id="made-by"></p>
+<p id="story">     <?php include "story.txt" ?></p>
+<br></br>
+<p id="author">by <?php include "author.txt" ?></p>
+<br></br>
+<br></br>
+<p id="made-by"> Made in 2020 by <a href="https://gnerikuser.github.io">gnerikuser</a>. </p>
