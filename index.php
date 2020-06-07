9a5583e9ddfe9dd567cc971b36518fae0b63b85c
Lucas <57920717+564online@users.noreply.github.com>
2020-06-07 17:58:26 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
-<title><?php include "title.txt" ?> by <?php include "author.txt" ?></title>
-<link rel="icon" href="stuff/favicon.ico">
-<link rel="stylesheet" type="text/css" href="stuff/css.css">
-<br></br>
-<br></br>
-<p id="title">     <?php include "Title.txt" ?></p>
-<p id="made-by"></p>
-<p id="story">     <?php include "Story.txt" ?></p>
-<br></br>
-<p id="author"> by <a href=" <?php include "Author Webpage.txt" ?> "><?php include "Author.txt" ?></a> </p>
-<br></br>
-<br></br>
-<p id="copyright"> <a href=" <?php include "Publisher Webpage.txt" ?> "><?php include "Text Copyright.txt" ?></a> </p>
-<br></br>
-<br></br>
-<p id="made-by"> Change-the-Text is a creation made since 2020-2020 by <a href="https://gnerikuser.github.io">gnerikuser</a>. </p>
+<html>
+    <head>
+        <title><?php include "Title.txt" ?> by <?php include "Author.txt" ?></title>
+        <link rel="icon" href="stuff/favicon.ico">
+        <link rel="stylesheet" type="text/css" href="stuff/css.css">
+        <br></br>
+        <br></br>
+    </head>
+    <body>
+        <p id="title">     <?php include "Title.txt" ?></p>
+        <p id="made-by"></p>
+        <p id="story">     <?php include "Story.txt" ?></p>
+        <br></br>
+        <p id="author"> by <a href=" <?php include "Author Webpage.txt" ?> "><?php include "Author.txt" ?></a> </p>
+        <br></br>
+        <br></br>
+        <p id="copyright"> <a href=" <?php include "Publisher Webpage.txt" ?> "><?php include "Text Copyright.txt" ?></a> </p>
+    </body>
+    <foot>
+        <br></br>
+        <br></br>
+        <p id="made-by"> Change-the-Text is a creation made since 2020-2020 by <a href="http://lucasmt.rf.gd">Lucas</a>. </p>
 
-<!-- Version 2 of Change-the-Text -->
-<!-- Created by gnerikuser in 2020 -->
-<!-- Thanks to Pinecone Arcade (Pinecone font creators), Carl Enlund (Helment font creator) and Vincenzo Vuono (Gravity font creator) -->
-<!-- https://github.com/gnerikuser/Change-the-Text -->
+        <!-- Version 2.0.1 of Change-the-Text -->
+        <!-- Created by Lucas M.T. in 2020 -->
+        <!-- Thanks to Pinecone Arcade (Pinecone font creators), Carl Enlund (Helment font creator) and Vincenzo Vuono (Gravity font creator) -->
+        <!-- https://github.com/564online/Change-the-Text -->
+    </foot>
+<html>
